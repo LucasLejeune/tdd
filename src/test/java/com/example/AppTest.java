b05fe2f85acbe56d223de9f73a5002e834bf0eb3
Lucas Lejeune <lucaslejeune0@gmail.com>
2024-06-12 10:57:20 +0200

    red

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,20 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+import java.util.List;
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void whenSearch_HasLessThanTwoCharacters() {
+        RechercheVille rechercheVille = new RechercheVille();
+
+        Assert.assertThrows(NotFound.class, () -> {
+            rechercheVille.rechercher("a");
+        } );
     }
 }
