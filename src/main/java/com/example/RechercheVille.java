0e9a23f5ed99d9471ab9e7ed89d2d55b8e367731
Lucas Lejeune <lucaslejeune0@gmail.com>
2024-06-12 10:48:03 +0200

    first commit

---
+++ src/main/java/com/example/RechercheVille.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.List;
+
+public class RechercheVille {
+    private List<String> villes;
+   
+   public List<String> rechercher(String mot)  {
+      throw new UnsupportedOperationException("Not implemented");
+   }
+}
