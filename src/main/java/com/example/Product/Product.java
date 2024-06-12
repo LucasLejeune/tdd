b83c5537195d039fec41dda9d72065a06d05ca87
Lucas Lejeune <lucaslejeune0@gmail.com>
2024-06-12 14:22:30 +0200

    Product first commit

---
+++ src/main/java/com/example/Product/Product.java
@@ -0,0 +1,23 @@
+package com.example.Product;
+
+public class Product {
+    String name = "";
+    Integer sellIn = 0;
+    Integer quality = 0;
+    String type = "";
+
+    public Integer getSellIn() {
+        return sellIn;
+    }
+    public void setSellIn(Integer sellIn) {
+        this.sellIn = sellIn;
+    }
+    public Integer getQuality() {
+        return quality;
+    }
+    public void setQuality(Integer quality) {
+        this.quality = quality;
+    }
+
+    
+}
---
+++ src/main/java/com/example/Product/Shop.java
@@ -0,0 +1,10 @@
+package com.example.Product;
+
+import java.util.List;
+    public class Shop {
+    private List<Product> products;
+    
+    public void update(Product product) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
